3a37510a97a6e3013a0ae4b13c9fbd2878a56585
Bryleo <bryleo2009@hotmail.com>
2024-10-15 18:59:37 -0500

    usuario-service init

---
+++ usuario-service/src/test/java/com/mcsv/usuario_service/UsuarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcsv.usuario_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
