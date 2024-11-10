d41f62b0f7184fbe14046830ecec94521e05bb3e
Bryleo <bryleo2009@hotmail.com>
2024-11-10 14:45:22 -0500

    login en memoria para Eureka

---
+++ P2/P2-Modular/eureka-server/src/main/java/com/mcsv/eureka_server/Config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.mcsv.eureka_server.Config;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${app.eureka.username}")
+    private String username;
+
+    @Value("${app.eureka.password}")
+    private String password;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());  // Usa la nueva sintaxis para evitar deprecación
+
+        return http.build();
+    }
+}
